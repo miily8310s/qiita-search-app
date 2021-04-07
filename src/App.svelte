7be375a6536cb61a9d0da5f1180600d80ab9b64f
miily8310s <rillakerika@gmail.com>
2021-04-07 22:34:35 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Header from './components/Header.svelte'
+	export let name: string;
+</script>
+
+<Header />
+<main>
+	<div class="container">
+    <h1>Hello {name}!</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</div>
+</main>
+
+<style>
+	main {
+		padding-top: 1rem;
+	}
+	h1 {
+		font-size: 1.2rem;
+	}
+	.container {
+		margin-left: auto;
+		margin-right: auto;
+		padding-left: 1.5rem;
+		padding-right: 1.5rem;
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,28 @@
+<header>
+  <div class="header-text">
+    <span>Qiita 記事一覧</span>
+  </div>
+</header>
+
+<style>
+  header {
+    --tw-bg-opacity: 1;
+    top: 0;
+    left: 0;
+    background-color: rgba(17, 24, 39, var(--tw-bg-opacity));
+    z-index: 40;
+  }
+  span {
+    color: #fff;
+    font-weight: 600;
+  }
+  .header-text {
+    width: 100%;
+    display: flex;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    height: 4rem;
+    align-items: center;
+    margin: auto;
+  }
+</style>
